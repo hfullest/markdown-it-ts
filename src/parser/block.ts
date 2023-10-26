cd659a712d7df4a18a81ca6b172f3120eaebd4c9
liuhuaifu <huafull0578@163.com>
2023-10-26 20:39:39 +0800

    feat: 添加 block rules

--- src/parser/block.ts
+++ src/parser/block.ts
@@ -1,8 +1,16 @@
-import { Ruler } from "../rules/ruler";
-import { StateBlock } from "../state/block";
+import { Ruler } from '../rules/ruler';
+import { StateBlock } from '../state/block';
+import tableFn from '../rules/block/table';
 
 export class ParserBlock {
-  ruler = new Ruler();
+  ruler = new Ruler([
+    { name: 'table', fn: tableFn, enabled: true, alt: ['paragraph', 'reference'] },
+    { name: 'code', fn: tableFn, enabled: true },
+    { name: 'fence', fn: tableFn, enabled: true, alt: ['paragraph', 'reference', 'blockquote', 'list'] },
+    { name: 'blockquote', fn: tableFn, enabled: true, alt: ['paragraph', 'reference', 'blockquote', 'list'] },
+    { name: 'hr', fn: tableFn, enabled: true, alt: ['paragraph', 'reference', 'blockquote', 'list'] },
+    { name: 'list', fn: tableFn, enabled: true, alt: ['paragraph', 'reference', 'blockquote'] },
+  ]);
 
   ruler2 = new Ruler();
 
