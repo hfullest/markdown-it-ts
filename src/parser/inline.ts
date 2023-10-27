2cbd23417672b0f296361b507910687c760a50d0
liuhuaifu <huafull0578@163.com>
2023-10-27 19:58:31 +0800

    feat: 添加 parser

--- src/parser/inline.ts
+++ src/parser/inline.ts
@@ -1,7 +1,9 @@
-import { Rule } from '../interface';
+import { EnvSandbox, Rule } from '../interface';
 import { Ruler } from '../rules/ruler';
 import { StateInline } from '../state/inline';
 import text from '../rules/inline/text';
+import { MarkdownIt } from '../markdown-it';
+import { Token } from '../basic/token';
 export class ParserInline {
   ruler = new Ruler<Rule.InlineRule>([
     { name: 'text', fn: text, enabled: true },
@@ -16,7 +18,7 @@ export class ParserInline {
    * Skip single token by running all rules in validation mode;
    * returns `true` if any rule reported success
    */
-  skipToken(state) {
+  skipToken(state: StateInline) {
     let ok,
       i,
       pos = state.pos,
@@ -66,5 +68,63 @@ export class ParserInline {
       state.pos++;
     }
     cache[pos] = state.pos;
+  }
+
+  /** Generate tokens for input range */
+  tokenize(state: StateInline) {
+    let ok,
+      i,
+      prevPos,
+      rules = this.ruler.getRules(''),
+      len = rules.length,
+      end = state.posMax,
+      maxNesting = state.md.options.maxNesting;
+
+    while (state.pos < end) {
+      // Try all possible rules.
+      // On success, rule should:
+      //
+      // - update `state.pos`
+      // - update `state.tokens`
+      // - return true
+      prevPos = state.pos;
+
+      if (state.level < maxNesting) {
+        for (i = 0; i < len; i++) {
+          ok = rules[i](state, false);
+          if (ok) {
+            if (prevPos >= state.pos) {
+              throw new Error("inline rule didn't increment state.pos");
+            }
+            break;
+          }
+        }
+      }
+
+      if (ok) {
+        if (state.pos >= end) {
+          break;
+        }
+        continue;
+      }
+
+      state.pending += state.src[state.pos++];
+    }
+
+    if (state.pending) {
+      state.pushPending();
+    }
+  }
+
+  /**
+   * ParserInline.parse(str, md, env, outTokens)
+   *
+   * Process input string and push inline tokens into `outTokens`
+   **/
+  parse(str: string, md: MarkdownIt, env: EnvSandbox, outTokens: Token[]) {
+    const state = new this.State(str, md, env, outTokens);
+    this.tokenize(state);
+    const rules = this.ruler2.getRules('');
+    rules.forEach((rule) => rule?.(state));
   }
 }
